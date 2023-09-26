cd240b1e851705dd8215346b5d410144490e5629
MLGSW4G <xxxmlgsw3gxxx@gmail.com>
2023-09-26 17:46:31 +0500

    v1.2

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from selenium.webdriver.support.ui import Select
 
 def read_config():
     try:
-        if getattr(sys, 'frozen', False):
+        if getattr(sys, 'frozen', False):  # For proper work with PyInstaller
             application_path = os.path.dirname(sys.executable)
         elif __file__:
             application_path = os.path.dirname(os.path.abspath(__file__))
@@ -17,7 +17,6 @@ def read_config():
 
         with open(config_path, 'r', encoding='UTF-8') as file:
             config = load(file)
-
         return config
     except Exception as e:
         print(f"Error reading config.json: {e}")
@@ -34,17 +33,16 @@ def create_dict_tr(tr):
 def tr_to_dict_tr(dict_tr):
     for key, value in dict_tr.items():
         return (f'{key: <23}{value[0]: <7}{value[1]: <10}{value[2]: <5}{value[3]: <9}{value[4]: <48}{value[5]: <4}'
-                f'{value[6]: <4}{value[7]: <35}')
+                f'{value[6]: <9}{value[7]: <35}')
 
 
 def tr_to_list(soup, number):
     tr = soup.find_all('tr', {'class': 'reportitem'})[number].text
     tr_list = tr.split('\n')[1:-1]
 
-    # Strip all the elements in the list
-    tr_list = [list_item.strip() for list_item in tr_list]
+    tr_list = [list_item.strip() for list_item in tr_list]  # Strip all the elements in the list
 
-    if len(tr_list[1]) < 5:
+    if len(tr_list[1]) < 5:  # If the time is less than 10:00, add a zero to its beginning
         tr_list[1] = '0' + tr_list[1]
 
     return tr_list
@@ -53,16 +51,29 @@ def tr_to_list(soup, number):
 def main():
     options = webdriver.ChromeOptions()
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    options.add_argument('--headless')  # Headless launch (No browser window opened)
+    # options.add_argument('--headless')  # Headless launch (No browser window opened)
     driver = webdriver.Chrome(options=options)
 
     driver.get(url)  # Load the web page
 
-    dropdown_element = driver.find_element(By.XPATH, "//*[@id='Name1']")  # Locate the <select> element by its ID
-    form_button = driver.find_element(By.XPATH, "/html/body/form/table/tbody/tr[3]/td[2]/table/tbody/tr[3]/td/input")
+    if date_from != "":
+        date_from_element = (driver.find_element
+                             (By.XPATH,"/html/body/form/table/tbody/tr[3]/td[2]/table/tbody/tr[1]/td[2]/input[1]"))
+        date_from_element.clear()
+        date_from_element.send_keys(date_from)
+
+    if date_to != "":
+        date_to_element = (driver.find_element
+                           (By.XPATH,"/html/body/form/table/tbody/tr[3]/td[2]/table/tbody/tr[1]/td[2]/input[4]"))
+        date_to_element.clear()
+        date_to_element.send_keys(date_to)
+
+    dropdown_element = driver.find_element(By.XPATH, "//*[@id='Name1']")
+    form_button = driver.find_element(By.XPATH,
+                                      "/html/body/form/table/tbody/tr[3]/td[2]/table/tbody/tr[3]/td/input")
 
     select = Select(dropdown_element)  # Create a Select instance
-    select.select_by_visible_text(group)  # select the desired option by its value
+    select.select_by_visible_text(group)  # Select the desired option by its value
 
     form_button.click()  # Click on the "Cформировать" button
 
@@ -72,11 +83,8 @@ def main():
 
     driver.quit()  # Cleanup and close the web driver
 
-    # Table headers
-    headers = ['Дата', 'Начало', 'Окончание', 'Пара', 'Группа', 'Дисциплина', 'Тип', 'Ауд', 'Преподаватель']
-
     print(f'{headers[0]: <23}{headers[1]: <7}{headers[2]: <10}{headers[3]: <5}{headers[4]: <9}{headers[5]: <48}'
-          f'{headers[6]: <4}{headers[7]: <4}{headers[8]: <35}')
+          f'{headers[6]: <4}{headers[7]: <9}{headers[8]: <35}')  # Print table headers
 
     #   Дата   Начало    Окончание   Пара    Группа    Дисциплина   Тип     Ауд     Преподаватель
     for tr in tr_elements_list:
@@ -89,9 +97,33 @@ def main():
 
 if __name__ == '__main__':
     config = read_config()
-    os.system(config['window_size'])
-    url = config['url']
-    group = config['group']
+    default_values = {
+        "window_size": "mode 150, 300",
+        "date_from": "",
+        "date_to": "",
+        "headers": [
+            "Дата",
+            "Начало",
+            "Окончание",
+            "Пара",
+            "Группа",
+            "Дисциплина",
+            "Тип",
+            "Ауд",
+            "Преподаватель"
+            ],
+        "url": "raspisanie.bf.pstu.ru",
+        "group": "ПОВТ-23д"
+    }
 
+    # Use default values if values from config file are empty
+    window_size = config.get('window_size', default_values['window_size']) or default_values['window_size']
+    date_from = config.get('date_from', default_values['date_from']) or default_values['date_from']
+    date_to = config.get('date_to', default_values['date_to']) or default_values['date_to']
+    headers = config.get('headers', default_values['headers']) or default_values['headers']
+    url = config.get('url', default_values['url']) or default_values['url']
+    group = config.get('group', default_values['group']) or default_values['group']
+
+    os.system(window_size)
     main()
     input('\nPress any key to exit...\n')
